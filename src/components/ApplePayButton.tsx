f809b5d9723f2103392c1583be2d35ff257f4809
Alex Austin <alex@goaro.com>
2025-06-19 15:28:45 -0400

    remove recurly from dependencies

--- src/components/ApplePayButton.tsx
+++ src/components/ApplePayButton.tsx
@@ -10,7 +10,7 @@ export const ApplePayButton: React.FC = () => {
     const [applePay, setApplePay] = React.useState<ApplePayInstance>()
 
     React.useEffect(() => {
-        if (!recurly)
+        if (!recurly || applePay)
             return
         
         const _checkoutPricing = recurly.Pricing.Checkout()
@@ -43,7 +43,7 @@ export const ApplePayButton: React.FC = () => {
         _applePay.ready(() => {
             setApplePay(_applePay)
         })
-    }, [recurly])
+    }, [applePay])
 
     const handleApplePay = () => {
         if (!recurly || !applePay)
